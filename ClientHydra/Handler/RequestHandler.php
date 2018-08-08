920065a6919d6636f68e4386dd3f45976862aca2
Boninir <raphael.eros.bonini@gmail.com>
2018-08-08 10:34:19 +0200

    fix error while getting no paginatable result.

--- ClientHydra/Handler/RequestHandler.php
+++ ClientHydra/Handler/RequestHandler.php
@@ -8,29 +8,22 @@ use Stadline\LinkdataClient\ClientHydra\Adapter\AdapterInterface;
 use Stadline\LinkdataClient\ClientHydra\Exception\HandlerException\HandlerException;
 use Stadline\LinkdataClient\ClientHydra\Exception\RequestException\RequestException;
 use Stadline\LinkdataClient\ClientHydra\Exception\SerializerException\SerializerException;
-use Stadline\LinkdataClient\ClientHydra\Type\HydraType;
-use Stadline\LinkdataClient\ClientHydra\Utils\Paginator;
 use Stadline\LinkdataClient\ClientHydra\Utils\Serializator;
-use Stadline\LinkdataClient\ClientHydra\Utils\UriConverter;
-use Stadline\LinkdataClient\Linkdata\Adapter\LinkdataAdapter;
 
 class RequestHandler
 {
     private $adapter;
     private $serializer;
-    private $uriConverter;
-    private $maxResultPerPage;
+    private $paginationHandler;
 
     public function __construct(
         AdapterInterface $adapter,
         Serializator $serializator,
-        UriConverter $uriConverter,
-        string $maxResultPerPage
+        PaginationHandler $paginationHandler
     ) {
         $this->adapter = $adapter;
         $this->serializer = $serializator;
-        $this->uriConverter = $uriConverter;
-        $this->maxResultPerPage = $maxResultPerPage;
+        $this->paginationHandler = $paginationHandler;
     }
 
     /**
@@ -40,6 +33,11 @@ class RequestHandler
     {
         $results = [[]];
         $content = $this->retrieveData($args);
+
+        if (\is_object($content)) {
+            return $content;
+        }
+
         $nbResult = $this->getNbResult($content);
 
         if ($nbResult > 1) {
@@ -47,7 +45,7 @@ class RequestHandler
             $nextPage = (int) $content['extra']['next_page'];
 
             while (0 !== $nextPage) {
-                $this->setNextPage($args, $nextPage);
+                $this->paginationHandler->setNextPage($args, $nextPage);
                 $content = $this->retrieveData($args);
 
                 $results[] = $content;
@@ -58,11 +56,7 @@ class RequestHandler
 
             unset($results['extra']);
 
-            $adapter = new LinkdataAdapter($results);
-            $paginator = new Paginator($adapter);
-            $paginator->setMaxPerPage($this->maxResultPerPage);
-
-            return $paginator;
+            return $this->paginationHandler->handlePagination($results);
         }
 
         if (1 === $nbResult) {
@@ -75,7 +69,7 @@ class RequestHandler
     /**
      * @throws HandlerException
      */
-    private function retrieveData(array $args): array
+    private function retrieveData(array $args)
     {
         try {
             $requestResponse = $this->adapter->makeRequest(
@@ -99,29 +93,9 @@ class RequestHandler
             return [];
         }
 
-        $extra = [
-            'first_page' => null,
-            'next_page' => null,
-            'last_page' => null,
-        ];
-
-        if ($this->serializer->hasNode($requestResponse, HydraType::VIEW)) {
-            $node = $this->serializer->getNodeValues($requestResponse, HydraType::VIEW);
-
-            $extra = [
-                'first_page' => $this->serializer->hasNode(\json_encode($node), HydraType::FIRST_PAGE) ?
-                    $this->uriConverter->getUriParam('page', $node[HydraType::FIRST_PAGE]) :
-                    null,
-                'next_page' => $this->serializer->hasNode(\json_encode($node), HydraType::NEXT_PAGE) ?
-                    $this->uriConverter->getUriParam('page', $node[HydraType::NEXT_PAGE]) :
-                    null,
-                'last_page' => $this->serializer->hasNode(\json_encode($node), HydraType::LAST_PAGE) ?
-                    $this->uriConverter->getUriParam('page', $node[HydraType::LAST_PAGE]) :
-                    null,
-            ];
+        if ($this->paginationHandler->haveToPaginate($content)) {
+            return $this->paginationHandler->addExtraNode($content, $requestResponse);
         }
-
-        $content['extra'] = $extra;
 
         return $content;
     }
@@ -131,18 +105,5 @@ class RequestHandler
         $response = \count($results);
 
         return \array_key_exists('extra', $results) ? $response - 1 : $response;
-    }
-
-    private function setNextPage(array &$args, int $nextPage): void
-    {
-        $page = $this->uriConverter->getUriParam('page', $args['uri']);
-
-        if (null === $page) {
-            $this->uriConverter->addUriParam('page', (string) $nextPage, $args['uri']);
-
-            return;
-        }
-
-        $this->uriConverter->updateUriParamValue('page', (string) $nextPage, $args['uri']);
     }
 }
