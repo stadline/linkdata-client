2b39e978cdb16eff031256fa031e4dae3157ea19
Thomas Glachant <thomas@glachant.fr>
2018-09-18 14:41:46 +0200

    Fix bad service name and config not loaded

--- Linkdata/Bridge/Symfony/Bundle/DependencyInjection/LinkdataClientExtension.php
+++ Linkdata/Bridge/Symfony/Bundle/DependencyInjection/LinkdataClientExtension.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Stadline\LinkdataClient\Linkdata\Bridge\Symfony\Bundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class LinkdataClientExtension extends Extension
@@ -18,5 +20,8 @@ class LinkdataClientExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('api.xml');
     }
 }
