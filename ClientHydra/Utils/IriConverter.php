c1ec183d09365cdcd26a1ac3a8493183a568bf7d
nicolasheraly <nhnicolasheraly@gmail.com>
2018-09-20 16:56:07 +0200

    wip IriConverter

--- ClientHydra/Utils/IriConverter.php
+++ ClientHydra/Utils/IriConverter.php
@@ -2,30 +2,37 @@
 
 namespace Stadline\LinkdataClient\ClientHydra\Utils;
 
+use Doctrine\Common\Inflector\Inflector;
 use Stadline\LinkdataClient\ClientHydra\Proxy\ProxyObject;
 
 class IriConverter
 {
-    private $baseUri;
+    private $baseUri = '/v2';
+    private $uriResolver;
+
+    public function __construct(UriResolver $uriResolver)
+    {
+        $this->uriResolver = $uriResolver;
+    }
 
     public function getIriFromObject(ProxyObject $object): string
     {
-
+        return sprintf('/%s/%s/%s', $this->baseUri, Inflector::pluralize($this->getClassShortName($object)), $object->getId());
     }
 
     public function getIriFromClassNameAndId(string $className, $id): string
     {
-
+        return sprintf('/%s/%s/%s', $this->baseUri, Inflector::pluralize($this->getClassShortName($className)), $id);
     }
 
     public function getCollectionIriFromClassName(string $className, $id): string
     {
-
+        return sprintf('/%s/%s', $this->baseUri, Inflector::pluralize($this->getClassShortName($className)));
     }
 
     public function getClassnameFromIri(string $iri): string
     {
-
+        return Inflector::singularize(explode('/', $iri)[2]);
     }
 
     public function generateCollectionUri(string $className, array $filters = []): string
@@ -38,8 +45,15 @@ class IriConverter
         $this->generateUri();
     }
 
-    protected function generateUri(string $baseUri, $parameters): string
+    protected function generateUri($parameters): string
     {
 
+    }
+
+    private function getClassShortName($classNameOrObject): string
+    {
+        $reflectionClass = new \ReflectionClass($classNameOrObject);
+
+        return $reflectionClass->getShortName();
     }
 }
