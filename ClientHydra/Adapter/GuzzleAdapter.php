cea4ccfbc25f9755cd6419983f6653b89b09f2fe
Thomas Glachant <thomas@glachant.fr>
2018-09-25 16:02:05 +0200

    Add cache to guzzle adapter

--- ClientHydra/Adapter/GuzzleAdapter.php
+++ ClientHydra/Adapter/GuzzleAdapter.php
@@ -15,6 +15,7 @@ class GuzzleAdapter implements AdapterInterface
     private $defaultHeaders = [
         'Content-Type' => 'application/ld+json',
     ];
+    private $cache;
 
     public function __construct(string $baseUrl)
     {
@@ -28,22 +29,28 @@ class GuzzleAdapter implements AdapterInterface
     {
         $headers = \array_merge($this->defaultHeaders, $headers);
 
-        try {
-            $response = $this->client->send(
-                new Request($method, $uri, $headers, $body)
-            );
-        } catch (GuzzleException $e) {
-            throw new RequestException(\sprintf('Error while requesting %s with %s method', $uri, $method), $body, $e);
+        $requestHash = \sha1(\json_encode($headers) . '.' . $method . '.' . $uri . '.' . $body);
+        if (!isset($this->cache[$requestHash])) {
+            try {
+                $response = $this->client->send(
+                    new Request($method, $uri, $headers, $body)
+                );
+                $this->cache[$requestHash] = $response;
+            } catch (GuzzleException $e) {
+                throw new RequestException(\sprintf('Error while requesting %s with %s method', $uri, $method), $body, $e);
+            }
+        } else {
+            $response = $this->cache[$requestHash];
         }
 
         $contentType = $response->getHeader('Content-Type')[0] ?? 'unknown';
         $contentType = \explode(';', $contentType)[0];
 
         if (\in_array($contentType, ['application/ld+json', 'application/json'], true)) {
-            return new JsonResponse((string) $response->getBody());
+            return new JsonResponse((string)$response->getBody());
         }
 
-        return new RawResponse($contentType, (string) $response->getBody());
+        return new RawResponse($contentType, (string)$response->getBody());
     }
 
     public function setDefaultHeader(string $key, string $value): void
