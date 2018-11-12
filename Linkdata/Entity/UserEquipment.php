205779fbcfcd8bc23459893c0567723880d94987
clementtalleu <clement.talleu@gmail.com>
2018-11-12 11:35:01 +0100

    replace private properties by public

--- Linkdata/Entity/UserEquipment.php
+++ Linkdata/Entity/UserEquipment.php
@@ -14,211 +14,61 @@ class UserEquipment extends ProxyObject
      *
      * @Groups({"equipment_norm"})
      */
-    private $id;
+    public $id;
 
     /**
      * @var User
      *
      * @Groups({"equipment_norm", "equipment_denorm"})
      */
-    private $user;
+    public $user;
 
     /**
      * @var string
      *
      * @Groups({"equipment_norm", "equipment_denorm"})
      */
-    private $category;
+    public $category;
 
     /**
      * @var string
      *
      * @Groups({"equipment_norm", "equipment_denorm"})
      */
-    private $brand;
+    public $brand;
 
     /**
      * @var string
      *
      * @Groups({"equipment_norm", "equipment_denorm"})
      */
-    private $name;
+    public $name;
 
     /**
      * @var array
      *
      * @Groups({"equipment_norm", "equipment_denorm"})
      */
-    private $sumups = [];
+    public $sumups = [];
 
     /**
      * @var Sport[]
      *
      * @Groups({"equipment_norm", "equipment_denorm"})
      */
-    private $sports;
+    public $sports;
 
     /**
      * @var \DateTime
      *
      * @Groups({"equipment_norm", "equipment_denorm"})
      */
-    private $createdAt;
+    public $createdAt;
 
     /**
      * @var \DateTime
      *
      * @Groups({"equipment_norm", "equipment_denorm"})
      */
-    private $updatedAt;
-
-    private $userIri;
-
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @return string|User
-     */
-    public function getUser()
-    {
-        return $this->hydrate($this->user);
-    }
-
-    public function getUserId(): string
-    {
-        if (!$this->userIri) {
-            $this->userIri = $this->user;
-        }
-
-        // Parse iri to get id.
-        return \explode('/', $this->userIri)[3];
-    }
-
-    public function getUserIri()
-    {
-        return $this->userIri;
-    }
-
-    /**
-     * @param User $user
-     */
-    public function setUser($user): void
-    {
-        $this->user = $user;
-    }
-
-    /**
-     * @return string
-     */
-    public function getCategory(): ?string
-    {
-        return $this->category;
-    }
-
-    /**
-     * @param string $category
-     */
-    public function setCategory($category): void
-    {
-        $this->category = $category;
-    }
-
-    /**
-     * @return string
-     */
-    public function getBrand(): ?string
-    {
-        return $this->brand;
-    }
-
-    /**
-     * @param string $brand
-     */
-    public function setBrand($brand): void
-    {
-        $this->brand = $brand;
-    }
-
-    /**
-     * @return string
-     */
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param string $name
-     */
-    public function setName($name): void
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * @return array
-     */
-    public function getSumups(): ?array
-    {
-        return $this->sumups;
-    }
-
-    /**
-     * @param array $sumups
-     */
-    public function setSumups($sumups): void
-    {
-        $this->sumups = $sumups;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getSports(): ?array
-    {
-        return $this->sports;
-    }
-
-    /**
-     * @param mixed $sports
-     */
-    public function setSports($sports): void
-    {
-        $this->sports = $sports;
-    }
-
-    /**
-     * @return \DateTime
-     */
-    public function getCreatedAt()
-    {
-        return $this->createdAt;
-    }
-
-    /**
-     * @param \DateTime $createdAt
-     */
-    public function setCreatedAt($createdAt): void
-    {
-        $this->createdAt = $createdAt;
-    }
-
-    /**
-     * @return \DateTime
-     */
-    public function getUpdatedAt()
-    {
-        return $this->updatedAt;
-    }
-
-    /**
-     * @param \DateTime $updatedAt
-     */
-    public function setUpdatedAt($updatedAt): void
-    {
-        $this->updatedAt = $updatedAt;
-    }
+    public $updatedAt;
 }
