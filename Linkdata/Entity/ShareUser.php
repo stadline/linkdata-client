5a49197873c07298a7bd216d88765851d2c3dfa0
GuillaumeCo <guillaume.copin@stadline.com>
2019-03-20 16:44:27 +0100

    ShareUser method annotations

--- Linkdata/Entity/ShareUser.php
+++ Linkdata/Entity/ShareUser.php
@@ -7,95 +7,40 @@ namespace Stadline\LinkdataClient\Linkdata\Entity;
 use Stadline\LinkdataClient\ClientHydra\Proxy\ProxyObject;
 use Symfony\Component\Serializer\Annotation\Groups;
 
+/**
+ * @method string getId()
+ * @method void   setId(string $id)
+ * @method User   getUser()
+ * @method void   setUser()
+ * @method string getCreatedAt()
+ * @method void   setCreatedAt(string $createdAt)
+ * @method string getUpdatedAt()
+ * @method void   setUpdatedAt(string $updatedAt)
+ */
 class ShareUser extends ProxyObject
 {
     /**
      * @var string
      * @Groups({"share_norm"})
      */
-    private $id;
+    public $id;
 
     /**
      * @var User
      *
      * @Groups({"share_norm", "share_denorm"})
      */
-    protected $user;
+    public $user;
 
     /**
      * @var string
      * @Groups({"share_norm"})
      */
-    private $createdAt;
+    public $createdAt;
 
     /**
      * @var string
      * @Groups({"share_norm"})
      */
-    private $updatedAt;
-
-    /**
-     * @return string
-     */
-    public function getId(): ?string
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param string $id
-     */
-    public function setId(string $id): void
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * @return string|User
-     */
-    public function getUser()
-    {
-        if (null !== $this->user) {
-            $this->user->_hydrate();
-        }
-
-        return $this->user;
-    }
-
-    public function setUser(?User $user): void
-    {
-        $this->user = $user;
-    }
-
-    /**
-     * @return string
-     */
-    public function getCreatedAt(): string
-    {
-        return $this->createdAt;
-    }
-
-    /**
-     * @param string $createdAt
-     */
-    public function setCreatedAt(string $createdAt): void
-    {
-        $this->createdAt = $createdAt;
-    }
-
-    /**
-     * @return string
-     */
-    public function getUpdatedAt(): string
-    {
-        return $this->updatedAt;
-    }
-
-    /**
-     * @param string $updatedAt
-     */
-    public function setUpdatedAt(string $updatedAt): void
-    {
-        $this->updatedAt = $updatedAt;
-    }
+    public $updatedAt;
 }
