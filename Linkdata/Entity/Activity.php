d8a4b5a5228c097cd4fed233dff18a28b8fff69b
GuillaumeCo <guillaume.copin@stadline.com>
2019-03-21 15:57:50 +0100

    Fix Activity

--- Linkdata/Entity/Activity.php
+++ Linkdata/Entity/Activity.php
@@ -8,60 +8,60 @@ use Stadline\LinkdataClient\ClientHydra\Proxy\ProxyObject;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
- * @method string      getId()
- * @method void        setId(string $id)
- * @method null|string getName()
- * @method void        setName(null|string $name)
- * @method User        getUser()
- * @method void        setUser(User $user)
- * @method Sport       getSport()
- * @method void        setSport(Sport $sport)
- * @method UserDevice  getUserDevice()
- * @method void        setUserDevice(UserDevice $userDevice)
- * @method \DateTime   getStartdate()
- * @method void        setStartdate(\DateTime $startdate)
- * @method int         getDuration()
- * @method void        setDuration(int $duration)
- * @method float       getLatitude()
- * @method void        setLatitude(float $latitude)
- * @method float       getLongitude()
- * @method void        setLongitude(float $longitude)
- * @method float       getElevation()
- * @method void        setElevation(float $elevation)
- * @method bool        isManual()
- * @method void        setManual(bool $manual)
- * @method string      getComment()
- * @method void        setComment(string $comment)
- * @method string      getConnector()
- * @method void        setConnector(string $connector)
- * @method string      getUserSession()
- * @method void        setUserSession(string $userSession)
- * @method array       getImages()
- * @method void        setImages(array $images)
- * @method bool        isCorrectedElevation()
- * @method void        setCorrectedElevation(bool $correctedElevation)
- * @method string      getThumbnail()
- * @method void        setThumbnail(string $thumbnail)
- * @method array       getDataSummaries()
- * @method void        setDataSummaries(array $dataSummaries)
- * @method array       getTags()
- * @method void        setTags(array $tags)
- * @method bool        isTrackFlag()
- * @method void        setTrackFlag(bool $trackFlag)
- * @method bool        isDatastreamFlag()
- * @method void        setDatastreamFlag(bool $datastreamFlag)
- * @method string      getGlobalChallenge()
- * @method void        setGlobalChallenge(string $globalChallenge)
- * @method array       getAvailableDatatypes()
- * @method void        setAvailableDatatypes(array $availableDatatypes)
- * @method array       getLocations()
- * @method void        setLocations(array $locations)
- * @method array       getDatastream()
- * @method void        setDatastream(array $datastream)
- * @method \DateTime   getCreatedAt()
- * @method void        setCreatedAt(\DateTime $createdAt)
- * @method \DateTime   getUpdatedAt()
- * @method void        setUpdatedAt(\DateTime $updatedAt)
+ * @method string           getId()
+ * @method void             setId(string $id)
+ * @method ?string          getName()
+ * @method void             setName(?string $name)
+ * @method User             getUser()
+ * @method void             setUser(User $user)
+ * @method Sport            getSport()
+ * @method void             setSport(Sport $sport)
+ * @method ?UserDevice      getUserDevice()
+ * @method void             setUserDevice(?UserDevice $userDevice)
+ * @method \DateTime        getStartdate()
+ * @method void             setStartdate(\DateTime $startdate)
+ * @method int              getDuration()
+ * @method void             setDuration(int $duration)
+ * @method ?float           getLatitude()
+ * @method void             setLatitude(?float $latitude)
+ * @method ?float           getLongitude()
+ * @method void             setLongitude(?float $longitude)
+ * @method ?float           getElevation()
+ * @method void             setElevation(?float $elevation)
+ * @method bool             isManual()
+ * @method void             setManual(bool $manual)
+ * @method ?string          getComment()
+ * @method void             setComment(?string $comment)
+ * @method ?Connector       getConnector()
+ * @method void             setConnector(?Connector $connector)
+ * @method ?UserSession     getUserSession()
+ * @method void             setUserSession(?UserSession $userSession)
+ * @method array            getImages()
+ * @method void             setImages(array $images)
+ * @method bool             isCorrectedElevation()
+ * @method void             setCorrectedElevation(bool $correctedElevation)
+ * @method ?string          getThumbnail()
+ * @method void             setThumbnail(?string $thumbnail)
+ * @method array            getDataSummaries()
+ * @method void             setDataSummaries(array $dataSummaries)
+ * @method array            getTags()
+ * @method void             setTags(array $tags)
+ * @method bool             isTrackFlag()
+ * @method void             setTrackFlag(bool $trackFlag)
+ * @method bool             isDatastreamFlag()
+ * @method void             setDatastreamFlag(bool $datastreamFlag)
+ * @method ?GlobalChallenge getGlobalChallenge()
+ * @method void             setGlobalChallenge(?GlobalChallenge $globalChallenge)
+ * @method array            getAvailableDatatypes()
+ * @method void             setAvailableDatatypes(array $availableDatatypes)
+ * @method array            getLocations()
+ * @method void             setLocations(array $locations)
+ * @method array            getDatastream()
+ * @method void             setDatastream(array $datastream)
+ * @method \DateTime        getCreatedAt()
+ * @method void             setCreatedAt(\DateTime $createdAt)
+ * @method \DateTime        getUpdatedAt()
+ * @method void             setUpdatedAt(\DateTime $updatedAt)
  */
 class Activity extends ProxyObject
 {
@@ -90,7 +90,7 @@ class Activity extends ProxyObject
     public $sport;
 
     /**
-     * @var UserDevice
+     * @var ?UserDevice
      * @Groups({"activity_norm", "activity_denorm"})
      */
     public $userDevice;
@@ -108,42 +108,43 @@ class Activity extends ProxyObject
     public $duration;
 
     /**
-     * @var float
+     * @var ?float
      * @Groups({"activity_norm", "activity_denorm"})
      */
     public $latitude;
 
     /**
-     * @var float
+     * @var ?float
      * @Groups({"activity_norm", "activity_denorm"})
      */
     public $longitude;
 
     /**
-     * @var float
+     * @var ?float
      * @Groups({"activity_norm", "activity_denorm"})
      */
     public $elevation;
 
     /**
      * @var bool
+     * @Groups({"activity_norm", "activity_denorm"})
      */
     public $manual = false;
 
     /**
-     * @var string
+     * @var ?string
      * @Groups({"activity_norm", "activity_denorm"})
      */
     public $comment;
 
     /**
-     * @var string
+     * @var ?Connector
      * @Groups({"activity_norm", "activity_denorm"})
      */
     public $connector;
 
     /**
-     * @var string
+     * @var ?UserSession
      * @Groups({"activity_norm", "activity_denorm"})
      */
     public $userSession;
@@ -161,7 +162,7 @@ class Activity extends ProxyObject
     public $correctedElevation = false;
 
     /**
-     * @var string
+     * @var ?string
      * @Groups({"activity_norm", "activity_denorm"})
      */
     public $thumbnail;
@@ -191,7 +192,7 @@ class Activity extends ProxyObject
     public $datastreamFlag = false;
 
     /**
-     * @var string
+     * @var ?GlobalChallenge
      * @Groups({"activity_norm", "activity_denorm"})
      */
     public $globalChallenge;
@@ -203,16 +204,12 @@ class Activity extends ProxyObject
     public $availableDatatypes = [];
 
     /**
-     * Temp storage for locations.
-     *
      * @var array
      * @Groups({"activity_norm"})
      */
     public $locations = [];
 
     /**
-     * Temp storage for datastream.
-     *
      * @var array
      * @Groups({"activity_norm"})
      */
