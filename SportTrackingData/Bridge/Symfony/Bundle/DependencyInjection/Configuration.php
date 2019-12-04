149d8b97f76a5d7a3b96edfda4df77e93aa84952
Thomas Glachant <thomas@glachant.fr>
2019-12-04 17:13:56 +0100

    Improve symfony configuration

--- SportTrackingData/Bridge/Symfony/Bundle/DependencyInjection/Configuration.php
+++ SportTrackingData/Bridge/Symfony/Bundle/DependencyInjection/Configuration.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace SportTrackingDataSdk\SportTrackingData\Bridge\Symfony\Bundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\ArrayNode;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -17,10 +17,11 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('sporttrackingdata');
+        $treeBuilder = new TreeBuilder();
 
-        /** @var ArrayNodeDefinition $rootNode */
-        $rootNode = $treeBuilder->getRootNode();
+        /** @var ArrayNode $rootNode */
+        $rootNode = $treeBuilder->root('sporttrackingdata');
+
         $rootNode
             ->children()
                 ->scalarNode('base_url')
