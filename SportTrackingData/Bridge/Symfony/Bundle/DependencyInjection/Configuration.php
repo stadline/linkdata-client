3a18aaff63bdb363b9ff3481998084a5dbada8da
Rémi <remi.bocquet@stadline.com>
2020-06-05 10:37:10 +0200

    fi: phpstan -> replace ArrayNode annotation

--- SportTrackingData/Bridge/Symfony/Bundle/DependencyInjection/Configuration.php
+++ SportTrackingData/Bridge/Symfony/Bundle/DependencyInjection/Configuration.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace SportTrackingDataSdk\SportTrackingData\Bridge\Symfony\Bundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\ArrayNode;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -19,7 +19,7 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder();
 
-        /** @var ArrayNode $rootNode */
+        /** @var ArrayNodeDefinition $rootNode */
         $rootNode = $treeBuilder->root('sporttrackingdata');
 
         $rootNode
